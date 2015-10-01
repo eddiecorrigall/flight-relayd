ad3e1e4e8c5882bcf1446673e1a821a4677af13d
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-10-01 14:49:09 -0400

    Cleaned up code, documented, and added removeListener

--- sse-demo/server.node.js
+++ sse-demo/server.node.js
@@ -2,28 +2,31 @@ var http = require('http');
 var events = require('events');
 
 var sbs1 = require('sbs1');
-var redis = require('redis');
 var express = require('express');
 
-function sse (message) {
+function SSE (message) {
 	/*
-	 {
-	  retry: <int>,
-	  id: <int>,
-	  event: <string>,
-	  data: <string|object>
-	 }
+	INPUT
+		message: {
+			id: <int>, // REQUIRED
+			retry: <int>, // optional
+			event: <string>, // optional
+			data: <string|object> // REQUIRED
+		}
+	OUTPUT
+		<string>
 	*/
 	if (typeof message !== 'object') return;
+	if (!message.id) return;
 	if (!message.data) return;
-	message.retry = parseInt(message.retry) || undefined;
 	message.id = parseInt(message.id) || undefined;
+	message.retry = parseInt(message.retry) || undefined;
 	var buffer = [];
-	if (0 <= message.retry) {
-		buffer.push('retry: ' + message.retry);
-	}
 	if (0 <= message.id) {
 		buffer.push('id: ' + message.id);
+	}
+	if (0 <= message.retry) {
+		buffer.push('retry: ' + message.retry);
 	}
 	if (typeof message.event === 'string') {
 		buffer.push('event: ' + message.event);
@@ -46,6 +49,19 @@ function sse (message) {
 	return buffer.join(NEWLINE);
 }
 
+function remap (f, mapping) {
+	// remap({'A':1,'B':2,'C':3}, {'a':'A','b':'B','c':'C','d':'D','d':'C'}) => {'a':1,'b':2,'c':3,'d':3}
+	var g = {};
+	for (var z in mapping) {
+		var x = mapping[z];
+		if (x in f) {
+			var y = f[x];
+			g[z] = y;
+		}
+	}
+	return g;
+}
+
 // ##### ##### ##### ##### ##### 
 // ##### Server
 // ##### ##### ##### ##### ##### 
@@ -57,37 +73,15 @@ if (port < 0) {
 	process.exit();
 }
 
-var app = express();
-var httpServer = http.createServer(app);
-var channel = new events.EventEmitter();
-
 var sbs1Client = sbs1.createClient({
 	host: '192.168.1.23'
 });
 
-/* WORKING EXAMPLE
-channel.on('broadcast', function (req, res) {
-	sbs1Client.on('message', function (msg) {
-		
-		if (msg.message_type !== sbs1.MessageType.TRANSMISSION) return;
-		if (msg.transmission_type !== sbs1.TransmissionType.ES_AIRBORNE_POS) return;
+var SSEChannel = new events.EventEmitter();
 
-		var icaco = msg.hex_ident;
-		if (!icaco) return;
+SSEChannel.on('join', function (req, res) {
 
-		var flightId = parseInt(icaco, 16);
-
-		res.write(sse({
-			'id': flightId,
-			'event': 'update',
-			'data': { time: msg.generated_time }
-		}));
-	});
-});
-*/
-
-channel.on('broadcast', function (req, res) {
-	sbs1Client.on('message', function (msg) {
+	var sbs1BoardcastListener = function (msg) {
 		switch (msg.message_type) {
 			case sbs1.MessageType.TRANSMISSION: { // information sent by aircraft
 
@@ -98,49 +92,57 @@ channel.on('broadcast', function (req, res) {
 
 				switch (msg.transmission_type) {
 					case sbs1.TransmissionType.ES_AIRBORNE_POS: {
-						res.write(sse({
-							'id': flightId,
+						var flightData = remap(msg, {
+							'date':	'generated_date',
+							'time':	'generated_time',
+							'lat':	'lat',
+							'lon':	'lon',
+							'alt':	'altitude'
+						});
+						res.write(SSE({
 							'event': 'position',
-							'data': {
-								'date': msg.generated_date,
-								'time': msg.generated_time,
-								'lat': msg.lat,
-								'lon': msg.lon,
-								'alt': msg.altitude
-							}
+							'id': flightId,
+							'data': flightData
 						}));
 					} break;
 					case sbs1.TransmissionType.ES_AIRBORNE_VEL: {
-						res.write(sse({
-							'id': flightId,
+						var flightData = remap(msg, {
+							'date':	'generated_date',
+							'time':	'generated_time',
+							'sx':	'ground_speed', // ground speed
+							'sy':	'vertical_rate' // climb rate
+						});
+						res.write(SSE({
 							'event': 'speed',
-							'data': {
-								'date': msg.generated_date,
-								'time': msg.generated_time,
-								'x': msg.ground_speed, // ground speed / horizontal speed
-								'y': msg.vertical_rate // rate of climb / vertical speed
-							}
+							'id': flightId,
+							'data': flightData
 						}));
 					} break;
 				}
 			} break;
 		}
+	};
+
+	// handle web browser closed connection
+	req.on('close', function() {
+		console.log('Browser connection closed');
+		sbs1Client.removeListener('message', sbs1BoardcastListener);
+		res.end();
 	});
+
+	// handle sbs1 messages
+	sbs1Client.addListener('message', sbs1BoardcastListener);
 });
 
-app.get('/flights', function (req, res) {
+var httpApp = express();
+var httpServer = http.createServer(httpApp);
+
+httpApp.get('/flights', function (req, res) {
 
 	console.log('Browser connection opened');
 
 	req.socket.setTimeout(0); // disabled existing timeout
 	req.socket.setNoDelay(true); // send data off immediately
-
-	// handle client connection close
-	req.on('close', function() {
-		console.log('Browser connection closed');
-		//sbs1Client.removeListener('message', sbs1Listener(req, res));
-		res.end();
-	});
 
 	// send sse headers
 	res.writeHead(200, {
@@ -150,10 +152,10 @@ app.get('/flights', function (req, res) {
 	});
 	res.write(':ok\n\n');
 
-	channel.emit('broadcast', req, res);
+	SSEChannel.emit('join', req, res);
 });
 
-app.use(express.static('./www'));
+httpApp.use(express.static('./www'));
 
 httpServer.listen(port, function () {
 	console.log('Server running at http://127.0.0.1:%s/', port);
