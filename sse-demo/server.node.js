83e5db1802ec013a8a4610ebe240daa83593ade2
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-10-02 18:32:46 -0400

    2nd iteration

--- sse-demo/server.node.js
+++ sse-demo/server.node.js
@@ -1,8 +1,18 @@
 var http = require('http');
 var events = require('events');
+var extend = require('util')._extend;
 
 var sbs1 = require('sbs1');
 var express = require('express');
+var cache = require('memory-cache');
+
+// ##### ##### ##### ##### ##### 
+// ##### Helper Functions
+// ##### ##### ##### ##### ##### 
+
+function getIPAddress (req) {
+	return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+}
 
 function SSE (message) {
 	/*
@@ -63,6 +73,132 @@ function remap (f, mapping) {
 }
 
 // ##### ##### ##### ##### ##### 
+// ##### SSE Channel
+// ##### ##### ##### ##### ##### 
+
+var SSEChannel = new events.EventEmitter();
+
+var FLIGHT_EXPIRE = 30*1000; // 30 seconds
+
+var sbs1Client = sbs1.createClient({
+	host: '192.168.1.23' // Set this to the SBS1 message address
+});
+
+SSEChannel.on('join', function (req, res) {
+
+	// ##### Setup SSE
+
+	console.log(getIPAddress(req) + ' - Browser connection opened');
+
+	req.socket.setTimeout(0); // disabled existing timeout
+	req.socket.setNoDelay(true); // send data off immediately
+
+	// ##### Send SSE headers
+	
+	res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	});
+
+	res.write(':ok\n\n');
+
+	// ##### Broadcast flight cache to new connections
+
+	cache.keys().forEach(function(flightId) {
+		var flightCache = cache.get(flightId);
+		if (flightCache === null) return;
+		res.write(SSE({
+			'event': 'flight_add',
+			'id': flightId,
+			'data': flightCache
+		}));
+	});
+
+	// ##### 
+
+	var sbs1BoardcastListener = function (msg) {
+		
+		switch (msg.message_type) {
+			
+			case sbs1.MessageType.TRANSMISSION: { // information sent by aircraft
+
+				var flightICACO = msg.hex_ident;
+				if (!flightICACO) return;
+
+				var flightId = parseInt(flightICACO, 16); // base 16 string to integer
+				var flightDelta = null;
+
+				switch (msg.transmission_type) {
+					
+					case sbs1.TransmissionType.ES_AIRBORNE_POS: {
+						// extract only valid information and rename
+						flightDelta = remap(msg, {
+							'lat': 'lat',
+							'lon': 'lon',
+							'alt': 'altitude'
+						});
+					} break;
+
+					case sbs1.TransmissionType.ES_AIRBORNE_VEL: {
+						flightDelta = remap(msg, {
+							'sx':	'ground_speed', // ground speed
+							'sy':	'vertical_rate' // climb rate
+						});
+					} break;
+				}
+
+				if (flightDelta) {
+
+					var flightEvent = 'flight_update';
+					var flightCache = cache.get(flightId);
+
+					if (flightCache === null) { // no prior knowledge
+						flightEvent = 'flight_add';
+						flightCache = {}
+					}
+
+					// extend flight cache with flight data, so that we can write this back into cache
+					extend(flightCache, flightDelta);
+
+					// debug information
+					console.log(flightICACO + ' => ' + JSON.stringify(flightCache));
+
+					// write into cache, setup automatic cleanup with timeout event
+					cache.put(flightId, flightCache, FLIGHT_EXPIRE, function (flightId) {
+						// send flight removed event
+						res.write(SSE({
+							'event': 'flight_remove',
+							'id': flightId,
+							'data': {} // No data to send
+						}));
+					});
+
+					// send flight event
+					res.write(SSE({
+						'event': flightEvent,
+						'id': flightId,
+						'data': flightDelta // only send the data that actually changed
+					}));
+				}
+			} break;
+		}
+	};
+
+	// ##### Handle web browser closed connection
+	
+	req.on('close', function() {
+		console.log(getIPAddress(req) + ' - Browser connection closed');
+		sbs1Client.removeListener('message', sbs1BoardcastListener);
+		res.end();
+	});
+
+	// ##### Handle SBS1 messages
+	
+	sbs1Client.addListener('message', sbs1BoardcastListener);
+});
+
+// ##### ##### ##### ##### ##### 
 // ##### Server
 // ##### ##### ##### ##### ##### 
 
@@ -73,85 +209,10 @@ if (port < 0) {
 	process.exit();
 }
 
-var sbs1Client = sbs1.createClient({
-	host: '192.168.1.23'
-});
-
-var SSEChannel = new events.EventEmitter();
-
-SSEChannel.on('join', function (req, res) {
-
-	var sbs1BoardcastListener = function (msg) {
-		switch (msg.message_type) {
-			case sbs1.MessageType.TRANSMISSION: { // information sent by aircraft
-
-				var icaco = msg.hex_ident;
-				if (!icaco) return;
-
-				var flightId = parseInt(icaco, 16);
-
-				switch (msg.transmission_type) {
-					case sbs1.TransmissionType.ES_AIRBORNE_POS: {
-						var flightData = remap(msg, {
-							'date':	'generated_date',
-							'time':	'generated_time',
-							'lat':	'lat',
-							'lon':	'lon',
-							'alt':	'altitude'
-						});
-						res.write(SSE({
-							'event': 'position',
-							'id': flightId,
-							'data': flightData
-						}));
-					} break;
-					case sbs1.TransmissionType.ES_AIRBORNE_VEL: {
-						var flightData = remap(msg, {
-							'date':	'generated_date',
-							'time':	'generated_time',
-							'sx':	'ground_speed', // ground speed
-							'sy':	'vertical_rate' // climb rate
-						});
-						res.write(SSE({
-							'event': 'speed',
-							'id': flightId,
-							'data': flightData
-						}));
-					} break;
-				}
-			} break;
-		}
-	};
-
-	// handle web browser closed connection
-	req.on('close', function() {
-		console.log('Browser connection closed');
-		sbs1Client.removeListener('message', sbs1BoardcastListener);
-		res.end();
-	});
-
-	// handle sbs1 messages
-	sbs1Client.addListener('message', sbs1BoardcastListener);
-});
-
 var httpApp = express();
 var httpServer = http.createServer(httpApp);
 
 httpApp.get('/flights', function (req, res) {
-
-	console.log('Browser connection opened');
-
-	req.socket.setTimeout(0); // disabled existing timeout
-	req.socket.setNoDelay(true); // send data off immediately
-
-	// send sse headers
-	res.writeHead(200, {
-		'Content-Type': 'text/event-stream',
-		'Cache-Control': 'no-cache',
-		'Connection': 'keep-alive'
-	});
-	res.write(':ok\n\n');
-
 	SSEChannel.emit('join', req, res);
 });
 
--- sse-demo/www/client.js
+++ sse-demo/www/client.js
@@ -1,22 +1,4 @@
-var flightEventsUrl = '/flights'; // can also be full address
-var flightEvents = new EventSource(flightEventsUrl, { withCredentials: true });
-
-flightEvents.onopen = function () {
-	console.log('connection opened');
-}
-
-flightEvents.onerror = function () {
-	console.error('connection error');
-}
-
-flightEvents.onmessage = function (e) { // generic message (has no event field)
-	var eventContent = {
-		'lastEventId': e.lastEventId,
-		'data': e.data,
-		'origin': e.origin
-	};
-	console.log('generic message: ' + JSON.stringify(eventContent));
-}
+// ##### 
 
 function dec2hex (x) {
 	x = parseInt(x);
@@ -27,14 +9,75 @@ function dec2hex (x) {
 	return undefined;
 }
 
-flightEvents.addEventListener('position', function(e) { // specific message (has event field)
-	var flightICACO = dec2hex(e.lastEventId);
-	var flightData = JSON.parse(e.data);
-	console.log('[' + flightICACO + ']' + ' - position - ' + JSON.stringify(flightData));
-}, false);
+// ##### 
 
-flightEvents.addEventListener('speed', function(e) { // specific message (has event field)
-	var flightICACO = dec2hex(e.lastEventId);
-	var flightData = JSON.parse(e.data);
-	console.log('[' + flightICACO + ']' + ' - speed - ' + JSON.stringify(flightData));
-}, false);
+var Toronto = new google.maps.LatLng(43.6568774, -79.32085);
+
+google.maps.event.addDomListener(window, 'load', function () {
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 8,
+		center: Toronto,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	});
+
+	// ##### SSE
+
+	var flightMarkers = {};
+	var flightEventsUrl = '/flights'; // can also be full address
+	var flightEvents = new EventSource(flightEventsUrl, { withCredentials: true });
+
+	flightEvents.onopen = function () {
+		console.log('connection opened');
+	}
+
+	flightEvents.onerror = function () {
+		console.error('connection error');
+	}
+
+	flightEvents.addEventListener('flight_add', function(e) { // specific message (has event field)
+		var flightId = ('' + e.lastEventId);
+		var flightICACO = dec2hex(flightId);
+		var flightData = JSON.parse(e.data);
+		console.log('[' + flightICACO + ']' + ' - add - ' + JSON.stringify(flightData));
+		// ...
+		if (flightData.lat === undefined) return;
+		if (flightData.lon === undefined) return;
+		var flightPosition = new google.maps.LatLng(flightData.lat, flightData.lon);
+		// ...
+		var infowindow = new google.maps.InfoWindow({
+			content: 'icaco: ' + flightICACO
+		});
+		var marker = new google.maps.Marker({
+			map: map,
+			position: flightPosition
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map, marker);
+		});
+		flightMarkers[flightId] = marker;
+	});
+
+	flightEvents.addEventListener('flight_update', function(e) {
+		var flightId = ('' + e.lastEventId);
+		var flightICACO = dec2hex(flightId);
+		var flightData = JSON.parse(e.data);
+		console.log('[' + flightICACO + ']' + ' - update - ' + JSON.stringify(flightData));
+		// ...
+		if (flightData.lat === undefined) return;
+		if (flightData.lon === undefined) return;
+		var flightPosition = new google.maps.LatLng(flightData.lat, flightData.lon);
+		// ...
+		flightMarkers[flightId].setPosition(flightPosition); // TODO: sometimes the marker does not exist, find out why
+	});
+
+	flightEvents.addEventListener('flight_remove', function(e) {
+		var flightId = ('' + e.lastEventId);
+		var flightICACO = dec2hex(flightId);
+		var flightData = JSON.parse(e.data);
+		console.log('[' + flightICACO + ']' + ' - remove - ' + JSON.stringify(flightData));
+		// ...
+		flightMarkers[flightId].setMap(null); // TODO: sometimes the marker does not exist, find out why
+		delete flightMarkers[flightId];
+	});
+});
